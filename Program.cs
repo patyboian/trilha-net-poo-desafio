a12a66513df930d8f910f2274bc111478051e9d7
Patricia Boian <patyboian86@gmail.com>
2025-11-18 13:31:20 -0300

    Subindo os arquivos modificados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone ("123", "123", "123", 28);
+iphone.Ligar();
+iphone.InstalarAplicativo("Wpp");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia ("321", "321", "321", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo");
